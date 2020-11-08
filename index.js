aab2d25f99cbb0d8253194195b32ee248add5da1
tranhd98 <62961553+tranhd98@users.noreply.github.com>
2020-11-08 00:31:21 -0800

    updated index

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const hostname = 'localhost';
-const port = process.env.PORT || 5000;
+const port = 5000;
 
 var friends = require("./friends.json"); // Once for all times
 
