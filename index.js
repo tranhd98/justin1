b914d38320e83fccc21ed01cd3bcf15899fedd52
tranhd98 <62961553+tranhd98@users.noreply.github.com>
2020-11-07 23:53:05 -0800

    updated index

--- index.js
+++ index.js
@@ -38,7 +38,6 @@ const server = http.createServer((request, response) => {
         '                                       <th scope="col">First Name</th> \n' +
         '                                       <th scope="col">Last Name</th> \n' +
         '                                       <th scope="col">Phone</th> \n' +
-        '                                       <th scope="col">Gender</th> \n' +
         '                               </tr> \n' +
         '                       </thead> \n' +
         '                       <tbody> \n'
@@ -50,7 +49,6 @@ const server = http.createServer((request, response) => {
                 '                                       <td>' + friends[key][f]["firstName"] + '</td> \n' +
                 '                                       <td>' + friends[key][f]["lastName"] + '</td> \n' +
                 '                                       <td>' + friends[key][f]["phone"] + '</td> \n' +
-                '                                       <td>' + friends[key][f]["gender"] + '</td> \n' +
                 '                               </tr> \n'
             );
 
